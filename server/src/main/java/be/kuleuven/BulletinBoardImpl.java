f4965c5abf3eb1ef5db13c18db3a4e733ea176e0
Reinout Annaert <reinout.annaert@student.kuleuven.be>
2023-11-12 13:50:26 +0100

    bulletinboard implementing interface

--- server/src/main/java/be/kuleuven/BulletinBoardImpl.java
+++ server/src/main/java/be/kuleuven/BulletinBoardImpl.java
@@ -1,32 +1,63 @@
 package be.kuleuven;
 
+import be.kuleuven.Instances.*;
+
 import java.rmi.*;
-import java.rmi.server.UnicastRemoteObject;
+import java.rmi.server.*;
 import java.security.*;
 
 public class BulletinBoardImpl extends UnicastRemoteObject implements BulletinBoardInterface{
 
-    public BulletinBoardImpl() throws RemoteException {
+    private static final int NUM_MAILBOXES = 64;
+    // Host name for the RMI registry
+    private static final String HOST_NAME = "localhost";
+    // RMI service name
+    private static final String SERVICE = "chat";
 
+    private static Mailbox[] bulletinBoard;
+
+    public BulletinBoardImpl() throws RemoteException {
+        BulletinBoardImpl.bulletinBoard = new Mailbox[NUM_MAILBOXES];
     }
 
     @Override
     public byte[] getMessage(int boxNumber, byte[] tag) throws NoSuchAlgorithmException, RemoteException {
-        return new byte[0];
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] hashedTag = messageDigest.digest(tag);
+        Mailbox targetMailbox = BulletinBoardImpl.bulletinBoard[boxNumber];
+        return targetMailbox.getMessageByTag(hashedTag);
     }
 
     @Override
     public void postMessage(int boxNumber, byte[] message, byte[] hashedTag) throws RemoteException {
-
+        // Store the message in the specified mailbox using the hashed tag
+        bulletinBoard[boxNumber].storeMessage(hashedTag, message);
     }
 
     @Override
     public void leave(String clientName) throws RemoteException {
-
+        System.err.println(clientName + " has left.");
     }
 
     @Override
     public int getAmountOfMailboxes() throws RemoteException {
-        return 0;
+        return bulletinBoard.length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Bulletin Board Information:\n");
+        sb.append("Host Name: ").append(HOST_NAME).append("\n");
+        sb.append("Service Name: ").append(SERVICE).append("\n");
+        sb.append("Number of Mailboxes: ").append(NUM_MAILBOXES).append("\n");
+        sb.append("Bulletin Board Status:\n");
+
+        for (int i = 0; i < NUM_MAILBOXES; i++) {
+            sb.append("Mailbox ").append(i).append(": ");
+            sb.append(bulletinBoard[i].toString()).append("\n");
+        }
+
+        return sb.toString();
     }
 }
