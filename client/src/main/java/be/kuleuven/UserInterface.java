fc9f5d335e75d95c1af3f22f5e96cc1ee5245d1b
Reinout Annaert <reinout.annaert@student.kuleuven.be>
2023-11-12 18:02:03 +0100

    started on joining the chat

--- client/src/main/java/be/kuleuven/UserInterface.java
+++ client/src/main/java/be/kuleuven/UserInterface.java
@@ -3,8 +3,11 @@ package be.kuleuven;
 import javax.swing.*;
 
 public class UserInterface extends JFrame{
+    // attributes
+    private String clientName;
 
-    private JFrame jFrame;
+
+    // javax.swing
     private JPanel panel;
     private JTextField userListTextField;
     private JTextField usernameTextField;
@@ -14,6 +17,7 @@ public class UserInterface extends JFrame{
     private JButton bumpButton;
     private JButton bumpBackButton;
     private JButton leaveButton;
+    private JButton sendMessageButton;
 
     public UserInterface(String title) {
         super(title);
@@ -32,9 +36,8 @@ public class UserInterface extends JFrame{
         bumpButton.setVisible(true);
         bumpBackButton.setVisible(true);
         leaveButton.setVisible(true);
+        sendMessageButton.setVisible(true);
     }
-
-
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
@@ -42,4 +45,28 @@ public class UserInterface extends JFrame{
             userInterface.setVisible(true);
         });
     }
+
+    // ***************** ButtonsClicks ************************
+
+    public void handleJoinButtonClick() {
+        clientName = usernameTextField.getText().toLowerCase();
+        if(!clientName.isEmpty()) {
+            clearUsernameTextField();
+            setButtonsEnabled(false, true, true, true, true);
+        }
+
+    }
+
+    public void clearUsernameTextField() {
+        usernameTextField.setText("");
+    }
+
+    public void setButtonsEnabled(boolean joinButton, boolean leaveButton, boolean bumpButton, boolean bumpBackButton, boolean sendMessageButton) {
+        this.joinButton.setEnabled(joinButton);
+        this.leaveButton.setEnabled(leaveButton);
+        this.bumpButton.setEnabled(bumpButton);
+        this.bumpBackButton.setEnabled(bumpBackButton);
+        this.sendMessageButton.setEnabled(sendMessageButton);
+    }
+
 }
