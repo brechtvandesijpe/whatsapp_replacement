bc8a01e4c68f6acea53e72fcb8e727fbf1d48ef7
Reinout Annaert <reinout.annaert@student.kuleuven.be>
2023-11-12 14:47:45 +0100

    client changes

--- client/src/main/java/be/kuleuven/Client.java
+++ client/src/main/java/be/kuleuven/Client.java
@@ -1,4 +1,31 @@
 package be.kuleuven;
 
+import be.kuleuven.Interfaces.BulletinBoardInterface;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class Client {
+
+    public BulletinBoardInterface bulletinBoardInterface;
+    private final UserInterface userInterface;
+    private String clientName;
+
+    public Client(String clientName, UserInterface userInterface){
+        this.clientName = clientName;
+        this.userInterface = userInterface;
+    }
+
+    public void connectToRMIServer() throws RemoteException {
+        try {
+            bulletinBoardInterface = (BulletinBoardInterface) Naming.lookup("rmi://localhost/groupChat");
+            System.out.println("Verbonden met de RMI-server.\n");
+        } catch (RemoteException | NotBoundException | MalformedURLException e) {
+            System.err.println("Fout bij verbinden met de RMI-server:");
+            e.printStackTrace();
+        }
+    }
+
 }
